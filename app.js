130e3895767a59407550d358a6de0b9225af70d8
pepnochizzy <laragray@live.co.uk>
2025-12-04 13:53:35 +0000

    feat: add data storage pseudo-code

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 console.log(`Hello World!`);
+
+//game logic
+//when the user clicks on the cookie, the total count of cookies goes up by 1
+//when the user clicks on the buy button in the shop/upgrade, the total count of cookies goes down by the cost of the upgrade, and the cps value goes up.
+
+//we will need functions to contain game logic
+//to create the logic for shop upgrades:
+//option 1: a function to handle each upgrade
+//option 2: a reusable function that works for every upgrade --this one (iterates for length of object array?)
+
+//tip on local storage: make sure the local storage values are updates after the user buys an upgrade OR when the user clicks on the 'cookie'
+
+//=================================================================================================================================
+
+//data storage
+let totalSnailCount = 0;
+let sps = 0; //snails per second, of course.
+
+// let stats = {
+//     totalSnailCount: 0,
+//     sps: 0,
+// }
+
+//if there is data already in local storage, update the stats in game with this data so the user picks it up where they left off!
+
+//================================================================================================================================
+
+//shop upgrade
+//fetch the upgrades from the API
+//create multiple DOM elements to contain the upgrades (loop)
+
+//TODO: create DOM elements for the shop upgrades
+//- create element
+//- assign value to its property (textContent)
+//- append it to DOM
+
+// after you complete this tast, you should see the upgrades in your shop-container :D
